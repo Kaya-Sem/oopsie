339d47f66e4c778c713581e4d5aff1403f1fd130
Kaya-Sem <kayasemvc@gmail.com>
2025-02-03 18:28:06 +0100

    update function header

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -13,7 +13,7 @@ func main() {
 
 	// Create an empty oopsie and customize it
 	customOops2 := oopsie.CreateOopsie()
-	customOops2.SetIndicatorColors(oopsie.BLACK, oopsie.GREEN)
+	customOops2.IndicatorColors(oopsie.BLACK, oopsie.GREEN)
 	customOops2.DisableIndicator(false)
 	customOops2.IndicatorMessage("200 OK")
 	customOops2.Title("connection succesfull")
@@ -23,7 +23,7 @@ func main() {
 
 	// Create a default oopsie
 	defaultOops := oopsie.CreateOopsie()
-	defaultOops.SetIndicatorColors(oopsie.BLACK, oopsie.BRIGHT_RED)
+	defaultOops.IndicatorColors(oopsie.BLACK, oopsie.BRIGHT_RED)
 	fmt.Println(defaultOops.Render()) // Output: error: an error occurred
 
 }
